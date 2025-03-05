a2fa17d7fe90f8501bad0b1ca1e09cd2907a3100
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-05 23:24:20 +0200

    displayed dynamic content inside jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+const reactDescriptions = ['Fundamental', 'Core', 'Crucial'];
+
+function randomInt(max) {
+	return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
 	return (
 		<header>
 			<img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
 			<h1>React Essentials</h1>
 			<p>
-				Fundamental React concepts you will need for almost any app you are
+				{reactDescriptions[randomInt(2)]} React concepts you will need for almost any app you are
 				going to build!
 			</p>
 		</header>
