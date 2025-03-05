f67ef98627152b4ab0bc62b74f1b4e6ebdc495b5
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-05 23:58:10 +0200

    used special import for image sources in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDescriptions = ['Fundamental', 'Core', 'Crucial'];
 
 function randomInt(max) {
@@ -6,10 +8,10 @@ function randomInt(max) {
 
 function Header() {
   const important = reactDescriptions[randomInt(2)];
-  
+
 	return (
 		<header>
-			<img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
+			<img src={reactImg} alt='Stylized atom' />
 			<h1>React Essentials</h1>
 			<p>
 				{important} React concepts you will need for almost any app you are
